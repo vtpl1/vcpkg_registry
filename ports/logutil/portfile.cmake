3df12eb7b9f235896fd239fe102b6df302a3e01d
Monotosh Das <monotosh.das@videonetics.com>
2022-05-31 17:51:09 +0530

    [logutil] new port

--- ports/logutil/portfile.cmake
+++ ports/logutil/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
-  REPO vtpl1/http_server
-  REF 42a36146656b269fb5310f2560260816c73f936f
-  SHA512 c7802ccc98e6a48cfb7d24b23c3a8be77d937ebab2b16855e9be58da91ad3c6cd6eefc6fb9dbbdc288e4152016bd736cca657bb88a8fb5cfeab2bc45b9b19b64
+  REPO vtpl1/logutil
+  REF v1.0.2
+  SHA512 0
   HEAD_REF main
 )
 
