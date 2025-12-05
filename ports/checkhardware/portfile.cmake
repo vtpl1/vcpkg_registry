2adbd6912c91a3e0f59d33d89398d8fa9eed0a46
Monotosh Das <monotosh.das@videonetics.com>
2025-12-05 06:38:23 +0000

    [queueutil] new port

--- ports/checkhardware/portfile.cmake
+++ ports/checkhardware/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
-  REPO vtpl1/queueutil
+  REPO vtpl1/check-hardware-
   REF "v${VERSION}"
-  SHA512 f1418376f78ebe8652db4930d7927ca9e37b7f3bf5235ab7218891bcd4ea4f7cfe86008205afeb80b969f798379d117ba3efe1acb69a33af770e37aedcbfcc5c
+  SHA512 1c0c63b4efaedb2d64db4ba71ffd8d1effac782cb9a0ef231bffa18c03490aef276082b0895d2675e5b88e00eaa1593c0eaefb6adb73f4cc7eae71c35a3d934d
   HEAD_REF main
 )
 
